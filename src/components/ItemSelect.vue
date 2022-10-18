df5c9ba8495aaa97a74ea48bcad6ffe458c6394c
feijinping <feijinping@foxmail.com>
2022-10-18 19:38:07 +0800

    s搜索修改

---
+++ src/components/ItemSelect.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+  .item-select-wrapper
+    el-select(
+      v-model="value1"
+      filterable
+      remote
+      clearable
+      :remote-method="doSearch"
+      :loading="loading"
+      )
+      el-option(
+        v-for="item in options"
+        :key="item.id"
+        :label="item.itemName"
+        :value="item.id"
+      )
+        div {{item.itemName}}
+        .info {{item.itemNo}}
+</template>
+
+<script>
+import { listWmsItem } from '@/api/wms/item'
+
+export default {
+  name: 'ItemSelect',
+  props: ['value'],
+  data() {
+    return {
+      options: [],
+      loading: false
+    }
+  },
+  computed: {
+    value1: {
+      get() {
+        return this.value
+      },
+      set(v) {
+        this.$emit('input', v)
+      }
+    }
+  },
+  created() {
+    this.doSearch();
+  },
+  methods: {
+    doSearch(search) {
+      listWmsItem({search}, {page: 0, size: 20}).then(res => {
+        const { content, totalElements } = res
+        this.options = content
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
