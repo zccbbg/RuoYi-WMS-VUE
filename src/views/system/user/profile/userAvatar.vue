bed870d1e9cd1c1d451d9a4603563385907f756f
chuzhichao <czc15706259078@163.com>
2023-05-04 10:22:00 +0800

    禁用头像修改

--- src/views/system/user/profile/userAvatar.vue
+++ src/views/system/user/profile/userAvatar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="user-info-head" @click="editCropper()"><img v-bind:src="options.img" title="点击上传头像" class="img-circle img-lg" /></div>
+    <div class="user-info-head"><img v-bind:src="options.img" title="点击上传头像" class="img-circle img-lg" /></div>
     <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body @opened="modalOpened"  @close="closeDialog">
       <el-row>
         <el-col :xs="24" :md="12" :style="{height: '350px'}">
