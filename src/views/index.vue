d54b72aef5cbb32455771356babf5036fe295d95
zccbbg <zccbbg@qq.com>
2023-06-12 17:34:26 +0800

    首页增加适配介绍

--- src/views/index.vue
+++ src/views/index.vue
@@ -12,6 +12,10 @@
       </div>
     </el-card>
     <el-row class="pl20 pr20" :gutter="10">
+      <el-col :span="24" style="padding-bottom: 20px;height:600px">
+          <iframe src="//player.bilibili.com/player.html?aid=996527481&high_quality=1"
+                  height="100%" width=100% scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+      </el-col>
       <el-col :span="12">
         <el-card shadow="always" style="padding-bottom: 20px;">
           <div>
@@ -367,11 +371,11 @@ export default {
   }
   .panel-group {
     margin-top: 18px;
-  
+
     .card-panel-col {
       margin-bottom: 32px;
     }
-  
+
     .card-panel {
       height: 108px;
       cursor: pointer;
@@ -382,37 +386,37 @@ export default {
       background: #fff;
       box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
       border-color: rgba(0, 0, 0, .05);
-  
+
       &:hover {
         .card-panel-icon-wrapper {
           color: #fff;
         }
-  
+
         .icon-people {
           background: #40c9c6;
         }
-  
+
         .icon-message {
           background: #36a3f7;
         }
-  
+
         .icon-money {
           background: #f4516c;
         }
       }
-  
+
       .icon-people {
         color: #40c9c6;
       }
-  
+
       .icon-message {
         color: #36a3f7;
       }
-  
+
       .icon-money {
         color: #f4516c;
       }
-  
+
       .card-panel-icon-wrapper {
         float: left;
         margin: 14px 0 0 14px;
@@ -420,43 +424,43 @@ export default {
         transition: all 0.38s ease-out;
         border-radius: 6px;
       }
-  
+
       .card-panel-icon {
         float: left;
         font-size: 48px;
       }
-  
+
       .card-panel-description {
         float: right;
         font-weight: bold;
         margin: 26px;
         margin-left: 0px;
-  
+
         .card-panel-text {
           line-height: 18px;
           color: rgba(0, 0, 0, 0.45);
           font-size: 16px;
           margin-bottom: 12px;
         }
-  
+
         .card-panel-num {
           font-size: 20px;
         }
       }
     }
   }
-  
+
   @media (max-width:550px) {
     .card-panel-description {
       display: none;
     }
-  
+
     .card-panel-icon-wrapper {
       float: none !important;
       width: 100%;
       height: 100%;
       margin: 0 !important;
-  
+
       .svg-icon {
         display: block;
         margin: 14px auto !important;
