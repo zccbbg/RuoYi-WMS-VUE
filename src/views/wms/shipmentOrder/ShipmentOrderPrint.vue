defa795fb840f7125f4457e13ec45d60ddbfd423
czc <czc15706259078@163.com>
2023-10-09 15:52:11 +0800

    出库打印不同分类物料分页

--- src/views/wms/shipmentOrder/ShipmentOrderPrint.vue
+++ src/views/wms/shipmentOrder/ShipmentOrderPrint.vue
@@ -9,56 +9,58 @@
       <!--      <div class="col1">日期: {{ row.createTime }}</div>-->
       <!--      <div class="col1">出库人: {{ row.createByName }}</div>-->
       <!--    </div>-->
-      <table class="common-table mb30" v-for="item in row">
-        <tr align="center">
-          <th :colspan="10">出库单：{{ item.shipmentOrderNo }}</th>
-        </tr>
-        <tr>
-          <th align="left">
-            顾客：
-          </th>
-          <th colspan="4" align="left">
-            {{ item.customerName }}
-          </th>
-          <th align="left">
-            操作员：
-          </th>
-          <th align="left">
-            {{ item.createByName }}
-          </th>
-          <th align="left">操作时间：</th>
-          <th align="left" colspan="2">{{ item.createTime || '' }}</th>
-        </tr>
-        <tr>
-          <th width="150">编号</th>
-          <th>名称</th>
-          <th>规格</th>
-          <th>数量</th>
-          <th>单位</th>
-          <th>单价</th>
-          <th>金额</th>
-          <th>批号</th>
-          <th width="120">效期</th>
-          <th>备注</th>
-        </tr>
-        <tr v-for="it in item.details">
-          <td align="center">{{ it.item.itemNo || '' }}</td>
-          <td align="center">{{ it.item.itemName || '' }}</td>
-          <td align="center">{{ it.item.specification || '' }}</td>
-          <td align="center">{{ it.planQuantity }}</td>
-          <td align="center">{{ it.item.unit || '' }}</td>
-          <td align="center">{{ it.money ? it.money : (it.money == 0 ? '0' : '') }}</td>
-          <td align="center">{{ it.money*it.planQuantity }}</td>
-          <td align="center">{{ it.item.batch || '' }}</td>
-          <td align="center">{{ parseTime(it.item.expiryDate, '{y}-{m}-{d}') }}</td>
-          <td align="center">{{ it.item.remark || '' }}</td>
-        </tr>
-        <tr>
-          <td colspan="4" align="center">合计：</td>
-          <td align="center" colspan="3">{{ digitalToChinese(item.amount) }}</td>
-          <td align="center" colspan="3">{{ item.amount }}元</td>
-        </tr>
-      </table>
+      <div v-for="item in row" style="height: 794px!important;">
+        <table class="common-table">
+          <tr align="center">
+            <th :colspan="10">出库单：{{ item.shipmentOrderNo }}</th>
+          </tr>
+          <tr>
+            <th align="left">
+              顾客：
+            </th>
+            <th colspan="4" align="left">
+              {{ item.customerName }}
+            </th>
+            <th align="left">
+              操作员：
+            </th>
+            <th align="left">
+              {{ item.createByName }}
+            </th>
+            <th align="left">操作时间：</th>
+            <th align="left" colspan="2">{{ item.createTime || '' }}</th>
+          </tr>
+          <tr>
+            <th width="150">编号</th>
+            <th>名称</th>
+            <th>规格</th>
+            <th>数量</th>
+            <th>单位</th>
+            <th>单价</th>
+            <th>金额</th>
+            <th>批号</th>
+            <th width="120">效期</th>
+            <th>备注</th>
+          </tr>
+          <tr v-for="it in item.details">
+            <td align="center">{{ it.item.itemNo || '' }}</td>
+            <td align="center">{{ it.item.itemName || '' }}</td>
+            <td align="center">{{ it.item.specification || '' }}</td>
+            <td align="center">{{ it.planQuantity }}</td>
+            <td align="center">{{ it.item.unit || '' }}</td>
+            <td align="center">{{ it.money ? it.money : (it.money == 0 ? '0' : '') }}</td>
+            <td align="center">{{ it.money*it.planQuantity }}</td>
+            <td align="center">{{ it.item.batch || '' }}</td>
+            <td align="center">{{ parseTime(it.item.expiryDate, '{y}-{m}-{d}') }}</td>
+            <td align="center">{{ it.item.remark || '' }}</td>
+          </tr>
+          <tr>
+            <td colspan="4" align="center">合计：</td>
+            <td align="center" colspan="3">{{ digitalToChinese(item.amount) }}</td>
+            <td align="center" colspan="3">{{ item.amount }}元</td>
+          </tr>
+        </table>
+      </div>
       <!--    <div class="foot">-->
       <!--      <div class="col2">备注: {{ row.remark }}</div>-->
       <!--    </div>-->
