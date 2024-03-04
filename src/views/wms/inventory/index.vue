62f24965699e3c124a3cd68f442bbef5420c7984
czc <czc15706259078@163.com>
2024-03-04 09:14:21 +0800

    库存看板界面重置按钮优化

--- src/views/wms/inventory/index.vue
+++ src/views/wms/inventory/index.vue
@@ -179,6 +179,8 @@ export default {
     /** 重置按钮操作 */
     resetQuery() {
       this.resetForm('queryForm')
+      this.queryParams.place = null
+      this.queryParams.quantityRange = null
       this.handleQuery()
     },
     // 多选框选中数据
