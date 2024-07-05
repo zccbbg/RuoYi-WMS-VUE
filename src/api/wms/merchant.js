922a84954f80123601727a2a840d7d42ef78c8a7
zccbbg <zccbbg@qq.com>
2024-07-05 12:58:54 +0800

    add merchant 自动生成的代码

---
+++ src/api/wms/merchant.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询往来单位列表
+export function listMerchant(query) {
+  return request({
+    url: '/wms/merchant/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询往来单位详细
+export function getMerchant(id) {
+  return request({
+    url: '/wms/merchant/' + id,
+    method: 'get'
+  })
+}
+
+// 新增往来单位
+export function addMerchant(data) {
+  return request({
+    url: '/wms/merchant',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改往来单位
+export function updateMerchant(data) {
+  return request({
+    url: '/wms/merchant',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除往来单位
+export function delMerchant(id) {
+  return request({
+    url: '/wms/merchant/' + id,
+    method: 'delete'
+  })
+}
---
+++ src/views/wms/merchant/index.vue
@@ -0,0 +1,306 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="编号" prop="merchantNo">
+        <el-input
+          v-model="queryParams.merchantNo"
+          placeholder="请输入编号"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="名称" prop="merchantName">
+        <el-input
+          v-model="queryParams.merchantName"
+          placeholder="请输入名称"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="Plus"
+          @click="handleAdd"
+          v-hasPermi="['wms:merchant:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="Edit"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['wms:merchant:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="Delete"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['wms:merchant:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="Download"
+          @click="handleExport"
+          v-hasPermi="['wms:merchant:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="merchantList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="" align="center" prop="id" v-if="false"/>
+      <el-table-column label="编号" align="center" prop="merchantNo" />
+      <el-table-column label="名称" align="center" prop="merchantName" />
+      <el-table-column label="地址" align="center" prop="address" />
+      <el-table-column label="级别" align="center" prop="merchantLevel" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+          <template #default="scope">
+              <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['wms:merchant:edit']">修改</el-button>
+              <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['wms:merchant:remove']">删除</el-button>
+          </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改往来单位对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="merchantRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="编号" prop="merchantNo">
+          <el-input v-model="form.merchantNo" placeholder="请输入编号" />
+        </el-form-item>
+        <el-form-item label="名称" prop="merchantName">
+          <el-input v-model="form.merchantName" placeholder="请输入名称" />
+        </el-form-item>
+        <el-form-item label="开户行" prop="bankName">
+          <el-input v-model="form.bankName" placeholder="请输入开户行" />
+        </el-form-item>
+        <el-form-item label="银行账户" prop="bankAccount">
+          <el-input v-model="form.bankAccount" placeholder="请输入银行账户" />
+        </el-form-item>
+        <el-form-item label="地址" prop="address">
+          <el-input v-model="form.address" placeholder="请输入地址" />
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="form.mobile" placeholder="请输入手机号" />
+        </el-form-item>
+        <el-form-item label="座机号" prop="tel">
+          <el-input v-model="form.tel" placeholder="请输入座机号" />
+        </el-form-item>
+        <el-form-item label="联系人" prop="contactPerson">
+          <el-input v-model="form.contactPerson" placeholder="请输入联系人" />
+        </el-form-item>
+        <el-form-item label="级别" prop="merchantLevel">
+          <el-input v-model="form.merchantLevel" placeholder="请输入级别" />
+        </el-form-item>
+        <el-form-item label="Email" prop="email">
+          <el-input v-model="form.email" placeholder="请输入Email" />
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入备注" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="Merchant">
+import { listMerchant, getMerchant, delMerchant, addMerchant, updateMerchant } from "@/api/wms/merchant";
+
+const { proxy } = getCurrentInstance();
+
+const merchantList = ref([]);
+const open = ref(false);
+const buttonLoading = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    merchantNo: undefined,
+    merchantName: undefined,
+  },
+  rules: {
+    id: [
+      { required: true, message: "不能为空", trigger: "blur" }
+    ],
+    merchantNo: [
+      { required: true, message: "编号不能为空", trigger: "blur" }
+    ],
+    merchantName: [
+      { required: true, message: "名称不能为空", trigger: "blur" }
+    ],
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询往来单位列表 */
+function getList() {
+  loading.value = true;
+  listMerchant(queryParams.value).then(response => {
+    merchantList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    merchantNo: null,
+    merchantName: null,
+    bankName: null,
+    bankAccount: null,
+    address: null,
+    mobile: null,
+    tel: null,
+    contactPerson: null,
+    merchantLevel: null,
+    email: null,
+    remark: null,
+    delFlag: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null
+  };
+  proxy.resetForm("merchantRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加往来单位";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  loading.value = true
+  reset();
+  const _id = row.id || ids.value
+  getMerchant(_id).then(response => {
+    loading.value = false;
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改往来单位";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["merchantRef"].validate(valid => {
+    if (valid) {
+      buttonLoading.value = true;
+      if (form.value.id != null) {
+        updateMerchant(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      } else {
+        addMerchant(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除往来单位编号为"' + _ids + '"的数据项？').then(function() {
+    loading.value = true;
+    return delMerchant(_ids);
+  }).then(() => {
+    loading.value = true;
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {
+  }).finally(() => {
+    loading.value = false;
+  });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('wms/merchant/export', {
+    ...queryParams.value
+  }, `merchant_${new Date().getTime()}.xlsx`)
+}
+
+getList();
+</script>
