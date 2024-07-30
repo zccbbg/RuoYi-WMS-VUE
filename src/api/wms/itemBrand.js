6a4fc137bfd6ca918ee3dc1bb6d2aa8dd11ee45e
DataCall <115401705@qq.com>
2024-07-30 11:38:24 +0800

    品牌管理

---
+++ src/api/wms/itemBrand.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询商品品牌列表
+export function listItemBrand(query) {
+  return request({
+    url: '/wms/itemBrand/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询商品品牌详细
+export function getItemBrand(id) {
+  return request({
+    url: '/wms/itemBrand/' + id,
+    method: 'get'
+  })
+}
+
+// 新增商品品牌
+export function addItemBrand(data) {
+  return request({
+    url: '/wms/itemBrand',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改商品品牌
+export function updateItemBrand(data) {
+  return request({
+    url: '/wms/itemBrand',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除商品品牌
+export function delItemBrand(id) {
+  return request({
+    url: '/wms/itemBrand/' + id,
+    method: 'delete'
+  })
+}
---
+++ src/views/wms/itemBrand/index.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <el-form :model="queryParams" ref="queryRef" :inline="true" label-width="68px">
+        <el-form-item label="品牌名称" prop="brandName">
+          <el-input
+            v-model="queryParams.brandName"
+            placeholder="请输入品牌名称"
+            clearable
+            @keyup.enter="handleQuery"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+          <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <el-card class="mt20">
+
+      <el-row :gutter="10" class="mb8" type="flex" justify="space-between">
+        <el-col :span="6"><span style="font-size: large">品牌列表</span></el-col>
+        <el-col :span="1.5">
+          <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['wms:itemBrand:add']"
+          >新增</el-button>
+          <el-button
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['wms:itemBrand:export']"
+          >导出</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table v-loading="loading" :data="itemBrandList" border class="mt20">
+        <el-table-column label="品牌名称" prop="brandName" />
+        <el-table-column label="创建时间" prop="createTime" width="180"/>
+        <el-table-column label="操作" align="right" class-name="small-padding fixed-width" width="180">
+          <template #default="scope">
+            <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['wms:itemBrand:edit']">修改</el-button>
+            <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['wms:itemBrand:remove']">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-row>
+        <pagination
+          v-show="total>0"
+          :total="total"
+          v-model:page="queryParams.pageNum"
+          v-model:limit="queryParams.pageSize"
+          @pagination="getList"
+        />
+      </el-row>
+
+    </el-card>
+    <!-- 添加或修改商品品牌对话框 -->
+    <el-drawer :title="title" v-model="open" size="50%" append-to-body>
+      <el-form ref="itemBrandRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="品牌名称" prop="brandName">
+          <el-input v-model="form.brandName" placeholder="请输入品牌名称" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-drawer>
+  </div>
+</template>
+
+<script setup name="ItemBrand">
+import { listItemBrand, getItemBrand, delItemBrand, addItemBrand, updateItemBrand } from "@/api/wms/itemBrand";
+import {ElMessageBox} from "element-plus";
+
+const { proxy } = getCurrentInstance();
+
+const itemBrandList = ref([]);
+const open = ref(false);
+const buttonLoading = ref(false);
+const loading = ref(true);
+const ids = ref([]);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    brandName: undefined,
+  },
+  rules: {
+    id: [
+      { required: true, message: "不能为空", trigger: "blur" }
+    ],
+    brandName: [
+      { required: true, message: "品牌名称不能为空", trigger: "blur" }
+    ],
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询商品品牌列表 */
+function getList() {
+  loading.value = true;
+  listItemBrand(queryParams.value).then(response => {
+    itemBrandList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    brandName: null,
+    createBy: null,
+    createTime: null,
+    updateBy: null,
+    updateTime: null
+  };
+  proxy.resetForm("itemBrandRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加商品品牌";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getItemBrand(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改商品品牌";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["itemBrandRef"].validate(valid => {
+    if (valid) {
+      buttonLoading.value = true;
+      if (form.value.id != null) {
+        updateItemBrand(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      } else {
+        addItemBrand(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        }).finally(() => {
+          buttonLoading.value = false;
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('确认删除品牌【' + row.brandName + '】吗？').then(function() {
+    return delItemBrand(_ids);
+  }).then(() => {
+    loading.value = true;
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch((e) => {
+    if (e === 409) {
+      return ElMessageBox.alert(
+        '<div>品牌【' + row.brandName + '】已有业务数据关联，不能删除 ！</div><div>请联系管理员处理！</div>',
+        '系统提示',
+        {
+          dangerouslyUseHTMLString: true,
+        }
+      )
+    }
+  }).finally(() => {
+    loading.value = false;
+  });
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('wms/itemBrand/export', {
+    ...queryParams.value
+  }, `itemBrand_${new Date().getTime()}.xlsx`)
+}
+
+getList();
+</script>
