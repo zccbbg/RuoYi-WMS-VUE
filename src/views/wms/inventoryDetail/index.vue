26c7839dedbfbb265eb45310463eebd015ddf726
zccbbg <zccbbg@qq.com>
2024-08-22 19:03:03 +0800

    refactor: 库存明细查询表单调整

--- src/views/wms/inventoryDetail/index.vue
+++ src/views/wms/inventoryDetail/index.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="app-container">
+    <el-card>
+
+      <el-form :model="queryParams" :inline="true" label-width="108px" style="display:flex;justify-content: space-between;flex-wrap: wrap">
+        <el-form-item label-width="90px" label="仓库" prop="warehouseId" class="col4">
+          <el-select v-model="queryParams.warehouseId" placeholder="请选择仓库" @change="handleChangeWarehouse"
+                     filterable clearable style="width:100%;">
+            <el-option v-for="item in useWmsStore().warehouseList" :key="item.id" :label="item.warehouseName"
+                       :value="item.id"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label-width="90px" label="库区" prop="areaId" class="col4">
+          <el-select v-model="queryParams.areaId" placeholder="请选择库区" :disabled="!queryParams.warehouseId || queryParams.type == 1" clearable
+                     filterable style="width:100%;">
+            <el-option v-for="item in useWmsStore().areaList.filter(it => it.warehouseId === queryParams.warehouseId)"
+                       :key="item.id" :label="item.areaName" :value="item.id"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="商品名称">
+          <el-input v-model="queryParams.itemName" clearable placeholder="商品名称"></el-input>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="商品编号">
+          <el-input class="w200" v-model="queryParams.itemCode" clearable placeholder="商品编号"></el-input>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="规格名称">
+          <el-input class="w200" v-model="queryParams.skuName" clearable placeholder="规格名称"></el-input>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="规格编号">
+          <el-input class="w200" v-model="queryParams.barcode" clearable placeholder="规格编号"></el-input>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="批号">
+          <el-input class="w200" v-model="queryParams.batchNo" clearable placeholder="批号"></el-input>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="过期" prop="daysToExpires">
+          <el-select v-model="queryParams.daysToExpires" clearable style="width:100%;">
+            <el-option label="30天内" :value="30">30天内</el-option>
+            <el-option label="60天内" :value="60">60天内</el-option>
+            <el-option label="90天内" :value="90">90天内</el-option>
+            <el-option label="120天内" :value="120">120天内</el-option>
+            <el-option label="180天内" :value="180">180天内</el-option>
+            <el-option label="365天内" :value="365">365天内</el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item class="col4" label-width="90px" label="入库日期" prop="createTimeRange">
+          <el-date-picker
+            v-model="queryParams.createTimeRange"
+            type="daterange"
+            range-separator="至"
+            value-format="YYYY-MM-DD HH:mm:ss"
+            format="YYYY-MM-DD"
+            :default-time="defaultTime"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="clickQuery">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
     <el-card class="mt20">
     <div class="mb8 flex-space-between">
       <div style="font-size: large">库存明细</div>
@@ -21,14 +80,12 @@
           <template #default="{ row }">
             <div>{{ row.item.itemName }}</div>
             <div v-if="row.item.itemCode">商品编号：{{ row.item.itemCode }}</div>
-            <div>{{ row.item.id }}</div>
           </template>
         </el-table-column>
         <el-table-column label="规格信息" prop="areaIdAndSkuId">
           <template #default="{ row }">
             <div>{{ row.itemSku.skuName }}</div>
             <div v-if="row.itemSku.skuCode">规格编号：{{ row.itemSku.skuCode }}</div>
-            <div>{{ row.itemSku.id }}</div>
           </template>
         </el-table-column>
       </template>
@@ -63,27 +120,28 @@
   </el-card>
   </div>
 </template>
+
 <script setup name="InventoryDetail">
 import {useWmsStore} from "@/store/modules/wms";
 import {computed, onMounted, reactive, ref} from "vue";
 import {listInventoryDetail} from "@/api/wms/inventoryDetail";
 import {getRowspanMethod} from "@/utils/getRowSpanMethod";
+import {ElForm} from "element-plus";
 
 const spanMethod = computed(() => getRowspanMethod(inventoryDetailList.value, rowSpanArray.value))
 const queryType = ref("warehouse");
 const inventoryDetailList = ref([]);
 const loading = ref(false);
 const rowSpanArray = ref(['warehouseId', 'areaId', 'areaIdAndItemId', 'areaIdAndSkuId'])
-const query = reactive({
+const queryParams = ref({
   itemName: '',
   itemCode: '',
   skuName: '',
   skuCode: '',
-  minQuantity: 1,
-  areaId: null,
-  warehouseId: null,
+  areaId: undefined,
+  warehouseId: undefined,
   batchNo: '',
-  daysToExpires: null
+  daysToExpires: undefined
 });
 const total = ref(0);
 const pageReq = reactive({
@@ -91,12 +149,18 @@ const pageReq = reactive({
   size: 10,
 });
 
+const handleChangeWarehouse = () => {
+  queryParams.value.areaId = undefined
+}
+
 onMounted(() => {
   getList();
 })
 
+
+
 const getList = () => {
-  const queryCopy = {...query}
+  const queryCopy = {...queryParams}
   if (queryCopy.createTimeRange) {
     queryCopy.createStartTime = queryCopy.createTimeRange[0]
     queryCopy.createEndTime = queryCopy.createTimeRange[1]
@@ -122,3 +186,8 @@ const getList = () => {
   }).finally(() => loading.value = false);
 }
 </script>
+<style scoped lang="scss">
+.col4 {
+  width: calc(24% - 1rem);
+}
+</style>
