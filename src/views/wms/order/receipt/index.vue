49e92c2f6c8e2512b1471d968b0bb501ea2fd38b
zccbbg <zccbbg@qq.com>
2024-09-25 15:40:22 +0800

    refactor: 入库列表样式优化

--- src/views/wms/order/receipt/index.vue
+++ src/views/wms/order/receipt/index.vue
@@ -149,16 +149,16 @@
             </div>
           </template>
         </el-table-column>
-        <el-table-column label="创建/更新" align="left">
-          <template #default="{ row }">
-            <div>创建：{{ row.createBy }}</div>
-            <div v-if="row.updateBy">更新：{{ row.updateBy }}</div>
-          </template>
-        </el-table-column>
-        <el-table-column label="创建时间/更新时间" align="left" width="200">
+        <el-table-column label="创建时间/更新时间" align="left" width="150">
           <template #default="{ row }">
             <div>创建：{{ parseTime(row.createTime, '{mm}-{dd} {hh}:{ii}') }}</div>
             <div>更新：{{ parseTime(row.updateTime, '{mm}-{dd} {hh}:{ii}') }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建/更新" align="left">
+          <template #default="{ row }">
+            <div>{{ row.createBy }}</div>
+            <div v-if="row.updateBy">{{ row.updateBy }}</div>
           </template>
         </el-table-column>
         <el-table-column label="备注" prop="remark" />
